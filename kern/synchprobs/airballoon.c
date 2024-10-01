de405bd7f585966db6049d6c095551285569f1af
Alex Sun <oscar8@sjtu.edu.cn>
2024-09-30 17:00:11 -0700

    Using two arrays to represent mappings between stakes and hooks. Have a buggy implementation of Dandelion and Marigold. Somehow they can still cut the severed rope. Also, I use a rope_mapping_lock, which is a big lock locking the whole flowerkiller, not neat at all.

--- kern/synchprobs/airballoon.c
+++ kern/synchprobs/airballoon.c
@@ -5,32 +5,24 @@
 #include <lib.h>
 #include <thread.h>
 #include <test.h>
+#include <synch.h>
 
 #define N_LORD_FLOWERKILLER 8
 #define NROPES 16
 static int ropes_left = NROPES;
 
+
 /* Data structures for rope mappings */
-typedef struct {
-	int hook_to_stake[NROPES];	 // mappings from hook to stake
-	int stake_to_hook[NROPES];	 // mappings from stake to hook
-	int hook_connected[NROPES];	 // 0 means hook disconnected from rope, 1 means otherwise
-	int stake_connected[NROPES]; // 0 means stake disconnected from rope, 1 means otherwise
-} RopeMapping
+int hook_to_stake[NROPES];	 // mappings from hook to stake (e.g. hook_to_stake[3] = 7 means hook 3 connects to stake 7)
+int stake_to_hook[NROPES];	 // mappings from stake to hook (e.g. stake_to_hook[7] = 3 means stake 7 connects to hook 3)
+int hook_connected[NROPES];	 // 0 means hook disconnected from rope, 1 means otherwise
+int stake_connected[NROPES]; // 0 means stake disconnected from rope, 1 means otherwise
 
-RopeMapping mapping;
-
-void initialize_ropemapping(RopeMapping *mapping) {
-    for (int i = 0; i < NROPES; i++) {
-        mapping->hook_to_stake[i] = i;
-        mapping->stake_to_hook[i] = i;
-        mapping->hook_connected[i] = 1;
-        mapping->stake_connected[i] = 1;
-    }
-}
 
 /* Synchronization primitives */
-
+static struct lock *rope_locks[NROPES]; // protect each rope
+static struct lock *ropes_left_lock;	// protect global variable ropes_left
+static struct lock *rope_mapping_lock;	// protect the stake-to-hook mapping
 
 
 /*
@@ -39,6 +31,35 @@ void initialize_ropemapping(RopeMapping *mapping) {
  * do all threads know when they are done?
  */
 
+
+/* Initialize rope mappings and locks. */
+static void initialize()
+{
+	for (int i = 0; i < NROPES; i++)
+	{
+		hook_to_stake[i] = i;
+		stake_to_hook[i] = i;
+		hook_connected[i] = 1;
+		stake_connected[i] = 1;
+		rope_locks[i] = lock_create("rope lock");
+	}
+	ropes_left_lock = lock_create("ropes_left lock");
+	rope_mapping_lock = lock_create("rope mapping lock");
+}
+
+
+/*
+ * Helper function:
+ * Check if the rope is cut.
+ * Should be protected by lock when being invoked.
+ */
+static bool is_rope_cut(int hook)
+{
+	return (!hook_connected[hook] || !stake_connected[hook_to_stake[hook]]);
+}
+
+
+/* Dandelion severs ropes from hooks. */
 static void dandelion(void *p, unsigned long arg)
 {
 	(void)p;
@@ -47,11 +68,46 @@ static void dandelion(void *p, unsigned long arg)
 	kprintf("Dandelion thread starting\n");
 
 	/* Implement this function */
+	while (true)
+	{
+		/* Check if all the ropes are severed. */
+		lock_acquire(ropes_left_lock);
+		if (ropes_left == 0)
+		{
+			lock_release(ropes_left_lock);
+			break;
+		}
+		lock_release(ropes_left_lock);
 
-	/* Dandelion succeeded unhooking one rope */
-	thread_yield();
+		/* Randomly select a hook. */
+		int hook = random() % NROPES;
+
+		/* Try to sever the rope. */
+		lock_acquire(rope_locks[hook]);
+		/* If the rope is not severed, sever it from the hook. */
+		if (!is_rope_cut(hook))
+		{
+			hook_connected[hook] = 0;
+
+			lock_acquire(ropes_left_lock);
+			ropes_left--;
+			kprintf("Dandelion severed rope %d\n", hook);
+			lock_release(ropes_left_lock);
+
+			lock_release(rope_locks[hook]);
+
+			/* Dandelion succeeded unhooking one rope. */
+			thread_yield();
+			continue;
+		}
+		lock_release(rope_locks[hook]);
+	}
+
+	kprintf("Dandelion thread done\n");
 }
 
+
+/* Marigold severs ropes from stakes. */
 static void marigold(void *p, unsigned long arg)
 {
 	(void)p;
@@ -60,6 +116,49 @@ static void marigold(void *p, unsigned long arg)
 	kprintf("Marigold thread starting\n");
 
 	/* Implement this function */
+	while (true)
+	{
+		/* Check if all the ropes are severed. */
+		lock_acquire(ropes_left_lock);
+		if (ropes_left == 0)
+		{
+			lock_release(ropes_left_lock);
+			break;
+		}
+		lock_release(ropes_left_lock);
+
+		/* Randomly select a stake. */
+		int stake = random() % NROPES;
+
+		/*
+		 * Mapping from stake to hook.
+		 * Acquire the corresponding hook from the selected stake,
+		 * protected by rope_mapping_lock.
+		 */
+		lock_acquire(rope_mapping_lock);
+		int hook = stake_to_hook[stake];
+		lock_release(rope_mapping_lock);
+
+		lock_acquire(rope_locks[hook]);
+		if (is_rope_cut(hook))
+		{
+			stake_connected[stake] = 0;
+			
+			lock_acquire(ropes_left_lock);
+			ropes_left--;
+			kprintf("Marigold severed rope %d from stake %d\n", hook, stake);
+			lock_release(ropes_left_lock);
+
+			lock_release(rope_locks[hook]);
+
+			/* Marigold succeeded unhooking one rope. */
+			thread_yield();
+			continue;
+		}
+		lock_release(rope_locks[hook]);
+	}
+
+	kprintf("Marigold thread done\n");
 }
 
 static void flowerkiller(void *p, unsigned long arg)
@@ -82,9 +181,9 @@ static void balloon(void *p, unsigned long arg)
 	/* Implement this function */
 
 	/* all ropes have been severed */
-	kprintf("Balloon freed and Prince Dandelion escapes!\n");
+	// kprintf("Balloon freed and Prince Dandelion escapes!\n");
 	/* balloon thread done*/
-	kprintf("Balloon thread done\n");
+	// kprintf("Balloon thread done\n");
 	/* then exit */
 }
 
@@ -98,7 +197,7 @@ int airballoon(int nargs, char **args)
 	(void)args;
 	(void)ropes_left;
 
-	initialize_ropemapping(mapping);
+	initialize();
 
 	err = thread_fork("Marigold Thread",
 					  NULL, marigold, NULL, 0);
