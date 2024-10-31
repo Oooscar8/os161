d2445e0c4f236c13d4439d4d522d5535461e4989
Jiayue Yang <yjiayue@student.ubc.ca>
2024-10-31 12:52:55 -0700

    fix bug in read and write (allow null buf now)

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -60,8 +60,8 @@ int sys_reboot(int code);
 int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 int sys_open(userptr_t *filename, int flags, mode_t mode, int *retval);
 int sys_close(int fd);
-int sys_read(int fd, userptr_t *buf, size_t buflen, int *retval);
-int sys_write(int fd, userptr_t *buf, size_t nbytes, int *retval);
+int sys_read(int fd, userptr_t buf, size_t buflen, int *retval);
+int sys_write(int fd, userptr_t buf, size_t nbytes, int *retval);
 off_t sys_lseek(int fd, off_t pos, int whence, off_t *retval);
 int sys_chdir(userptr_t *pathname, int *retval);
 int sys_dup2(int oldfd, int newfd, int *retval);
@@ -70,5 +70,6 @@ pid_t sys_fork(struct trapframe *tf, pid_t *retval);
 pid_t sys_getpid(pid_t *retval);
 void sys__exit(int exitcode);
 pid_t sys_waitpid(pid_t pid, int *status, int options, pid_t *retval);
+int sys_execv(const char *program, char **args);
 
 #endif /* _SYSCALL_H_ */
