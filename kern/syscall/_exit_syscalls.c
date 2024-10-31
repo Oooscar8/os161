8c2105b538e3d1886971627e2fcbd1fa6878908e
Alex Sun <oscar8@sjtu.edu.cn>
2024-10-31 06:28:38 +0000

    Solve parent exiting first by reassigning child to init process.

--- kern/syscall/_exit_syscalls.c
+++ kern/syscall/_exit_syscalls.c
@@ -3,7 +3,7 @@
  */
 void
 sys__exit(int exitcode)
-{   
+{  
     KASSERT(curproc != NULL);
     KASSERT(curproc != kproc);    /* Kernel process cannot exit */
     
@@ -13,6 +13,28 @@ sys__exit(int exitcode)
     /* Set exit code and change state to zombie */
     curproc->p_exitcode = _MKWAIT_EXIT(exitcode);
     curproc->p_state = PROC_ZOMBIE;
+
+    /* 
+     * Reassign all children to init process (PID 1)
+     * This is done before becoming a zombie so we maintain
+     * a consistent process hierarchy
+     */
+    struct proc *p;
+    spinlock_acquire(&pid_lock);
+    for (int i = 0; i < MAX_PROCS; i++) {
+        p = pid_table[i].proc;
+        if (p != NULL && p->p_parent == curproc) {
+            spinlock_acquire(&p->p_lock);         
+            p->p_parent = pid_table[PID_INIT].proc;
+            
+            /* If child is already zombie, signal init to collect it */
+            if (p->p_state == PROC_ZOMBIE) {
+                V(p->p_sem);
+            }
+            spinlock_release(&p->p_lock);
+        }
+    }
+    spinlock_release(&pid_lock);
 
     /* Signal any waiting parent */
     if (curproc->p_parent != NULL) {
